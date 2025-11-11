69ee398c567125a1090f54fe615a2cbfc74affa0
Talan Malicote <malicotl@miamioh.edu>
2025-11-11 15:19:49 -0500

    Update Location.java
    Updated author information and modified comments for clarity.

--- Location.java
+++ Location.java
@@ -1,20 +1,20 @@
 /**
  * Class: Location
-* @author Testimony Awuzie
-* @version 1.0
-* Course : CSE 201 Fall 2025
-* Written: November 5, 2025
-*
-* Purpose: – Location Class Definition
+ * 
+ * @author Talan Malicote
+ * @version 1.0 Course: CSE 201 Fall 2025 Written: November 10, 2025
+ *
+ *          Purpose: – Location Class Definition
  */
 public class Location {
 	private String locationName;
 	private Item items;
 	private Location subLocations;
-	
+
 	/**
-	 * Constructor for Location
-	 * Initializes a Location object with the specified Name, Items in it, and sublocations
+	 * Constructor for Location Initializes a Location object with the specified
+	 * Name, Items in it, and sublocations
+	 * 
 	 * @param locationName
 	 * @param items
 	 * @param subLocations
@@ -24,35 +24,92 @@ public class Location {
 		this.items = items;
 		this.subLocations = subLocations;
 	}
-	
+
 	/**
 	 * Search for Item in Location
-	 * @return
+	 * 
+	 * @return the Location containing the item, or null if not found
 	 */
-	public Location search () {
-		// TODO: 
+	public Location search() {
+		if (items != null) {
+			System.out.println("You found an item: " + item.getName());
+			return this;
+		}
+
+		if (subLocations != null) {
+			System.out.println("Searching sublocation: " + subLocations.getLocationName());
+			return subLocations.search();
+		}
+		
+		System.out.println("No items found in " + locationName + " or its sublocations.");
 		return null;
 	}
-	
+
 	/**
 	 * Adds Item to Location
+	 * If an item already exists, it will be replaced
+	 * 
+	 * @para item the item to add
 	 */
 	public void addItem(Item item) {
-		// TODO: 
-		// Test for Invalid Input
+		if (item == null) {
+			System.out.println("Cannot add a null item.");
+			return;
+		}
 		
+		if (this.items != null) {
+			System.out.println("Replacing existing item (" + this.item.getName() + ") with " item.getName() + ".");
+		} else {
+			System.out.println("Adding " + item.getName() + " to " + locationName + ".");
+		}
 		
-		// Add if Valid
+		this.items = item;
 	}
-	
+
 	/**
 	 * Removes Item from Location
-	 * @param itemName
-	 * @return item removed
+	 * 
+	 * @param itemName the name of the item to remove
+	 * @return item removed, or null if not found
 	 */
 	public Item removeItem(String itemName) {
-		// TODO:
+		if (items == null) {
+			System.out.println("There are no items in " + locationName + "to remove.");
+			return null;
+		}
+
+		if (items.getName().equals(itemName)) {
+			Item removedItem = items;
+			System.out.println("Removed " + removedItem.getName() + " from " + locationName + ".");
+			return removedItem;
+		}
+
+		if (subLocations != null) {
+			return subLocations.removeItem(itemName);
+		}
+
+		System.out.println(itemName + " not found in " + locationName + ".");
+		return null;
+
+	}
+	
+	/*
+	 *  Getters and Setters for Location, Item, and subLocation, 
+	 */
+
+	public String getLocationName() {
+		return locationName;
+	}
+
+	public Item getItems() {
 		return items;
-		
+	}
+
+	public Location getsubLocations() {
+		return subLocations;
+	}
+
+	public void setsubLocations(Location subLocations) {
+		this.subLocations = subLocations;
 	}
 }
