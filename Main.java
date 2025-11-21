c6ea5a23f98d1f0d4153075965f23a1aaef5414c
awuziets-sys <awuziets@miamioh.edu>
2025-11-21 12:55:18 -0500

    Refactor main method and variable naming

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
 
 public class Main {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        Game game = new Game();
-
-        game.startGame();
-    }
+	public static void main(String[] args) {
+		Game newGame = new Game();
+		
+		newGame.startGame();
+	}
 
 }
-
