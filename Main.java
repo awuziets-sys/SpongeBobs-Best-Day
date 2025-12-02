aa13ae03f7c6bf8676030abb5b9b67fe0fdd0f97
awuziets-sys <awuziets@miamioh.edu>
2025-12-01 21:43:42 -0500

    Add documentation comments to Main class
    Added class-level documentation for Main.

--- Main.java
+++ Main.java
@@ -1,4 +1,12 @@
-
+/**
+ * Class: Main
+ * @author Testimony Awuzie
+ * @version 1.0
+ * Course : CSE 201 Fall 2025
+ * Written: November 19, 2025
+ *
+ * Purpose: This class will run the Game.
+ */
 public class Main {
 
 	public static void main(String[] args) {
