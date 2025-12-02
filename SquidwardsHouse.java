3d766f6ac8948a7ddc262ae2620062665078b646
awuziets-sys <awuziets@miamioh.edu>
2025-12-01 21:48:00 -0500

    Refactor SquidwardsHouse interaction methods
    Updated the enterRoom and runRoom methods to include additional narrative and player interaction. Adjusted output messages for clarity and engagement.

--- SquidwardsHouse.java
+++ SquidwardsHouse.java
@@ -1,10 +1,8 @@
-import java.util.Scanner;
-
 /**
  * Class: SquidwardsHouse
- * Author: Nuria Jurjo
+ * @author: Nuria Jurjo
  * Course: CSE 201
- * 
+ *
  * Represents Squidward's house in the game.
  * The player must find the correct way to enter.
  * ONLY tunneling underground (option 3) will succeed.
@@ -14,94 +12,74 @@ public class SquidwardsHouse extends Room {
     // Tracks whether the player managed to enter successfully
     private boolean enteredSuccessfully = false;
 
-    /**
-     * Full constructor
-     */
     public SquidwardsHouse(String roomName, String roomDescription, boolean roomComplete, Room nextRoom) {
         super(roomName, roomDescription, roomComplete, nextRoom);
     }
 
-    /**
-     * Default constructor
-     */
     public SquidwardsHouse() {
         this("Squidward's House", "The second level of the game!", false, null);
     }
 
-    /**
-     * Called when the player first enters Squidward's House.
-     * This method prints the introduction and choices.
-     */
     @Override
     public void enterRoom(Player player) {
+        this.player = player;
+
         System.out.println("\nYou arrive at " + getRoomName() + ".");
         System.out.println(getRoomDescription());
-
-        // Give player choices
+        System.out.println("\nYou knock on the door. Silence.");
+        System.out.println("Worried, you decide to find a way inside to check on Squidward.");
         System.out.println("\nHow will you try to enter?");
         System.out.println("1. Blow up the door");
         System.out.println("2. Catapult yourself through the window");
         System.out.println("3. Tunnel underground to the living room");
-
         System.out.println("\n(Type 1, 2, or 3)");
     }
 
-    /**
-     * Main logic loop for this room.
-     * Keeps running until the correct choice is made.
-     */
     @Override
     public void runRoom() {
 
-        Scanner userInput = new Scanner(System.in);
-
-        while (!super.isRoomComplete()) {
-
+        while (!isRoomComplete()) {
             System.out.print("> ");
-            String input = userInput.nextLine().trim();
+            String inputLine = input.nextLine().trim();
 
-            switch (input) {
+            switch (inputLine) {
+            case "1":
+                System.out.println(
+                    "\nYou try to blow up the door. " +
+                    "The explosion backfires and you're covered in soot.\n" +
+                    "Squidward is NOT impressed (wherever he is)."
+                );
+                break;
 
-                case "1":
-                    System.out.println(
-                        "\nYou try to blow up the door. " +
-                        "The explosion backfires and you're covered in soot.\n" +
-                        "Squidward is NOT impressed."
-                    );
-                    break;
+            case "2":
+                System.out.println(
+                    "\nYou launch yourself with a catapult and slam into the window.\n" +
+                    "It doesn't break, but your dignity might have."
+                );
+                break;
 
-                case "2":
-                    System.out.println(
-                        "\nYou launch yourself with a catapult and crash into the window.\n" +
-                        "Ouch! That definitely didn’t work."
-                    );
-                    break;
+            case "3":
+                System.out.println(
+                    "\nYou dig a tunnel underground and pop up in Squidward’s living room."
+                );
+                System.out.println("Squidward: \"Barnacles! SPONGEBOB!! What are you doing in my house?!\"");
+                System.out.println("SpongeBob: \"I was just checking to see if you were okay!\"");
+                System.out.println("Squidward: \"I would be... IF YOU WEREN'T HERE!\"");
+                System.out.println("SpongeBob: \"Great! Off to see Patrick!\"");
+                System.out.println("*SpongeBob tunnels back out and heads toward Patrick’s Rock...*");
 
-                case "3":
-                    System.out.println(
-                        "\nYou dig a tunnel underground and pop up in Squidward’s living room."
-                    );
-                    System.out.println("Squidward: SPONGEBOB! What are you doing in my house?!");
-                    System.out.println("SpongeBob: I was just checking to see if you were okay, Squidward!");
-                    System.out.println("Squidward: I was. Until five seconds ago.");
-                    System.out.println("SpongeBob: Great! Have a SUPER sparkly day!");
-                    System.out.println("*SpongeBob happily tunnels back out*");
-                    // Mark as successful
-                    enteredSuccessfully = true;
+                enteredSuccessfully = true;
+                completeRoom();
+                System.out.println("\nType 'next' to travel to Patrick's Rock.");
+                return;
 
-                    // Mark the room complete using the parent method
-                    super.completeRoom();
-
-                    return;
-
-                default:
-                    System.out.println("Invalid choice. Please enter 1, 2, or 3.");
-                    break;
+            default:
+                System.out.println("Invalid choice. Please enter 1, 2, or 3.");
+                break;
             }
 
-            // If not complete yet, remind the user
-            if (!super.isRoomComplete()) {
-                System.out.println("\nTry a different method.");
+            if (!isRoomComplete()) {
+                System.out.println("\nTry a different method:");
                 System.out.println("1. Blow up the door");
                 System.out.println("2. Catapult yourself through the window");
                 System.out.println("3. Tunnel underground to the living room");
@@ -109,9 +87,6 @@ public class SquidwardsHouse extends Room {
         }
     }
 
-    /**
-     * Returns whether this room has been successfully completed.
-     */
     @Override
     public boolean checkCompletionOfRoom() {
         return enteredSuccessfully;
